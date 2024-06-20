c15fe35a47fdef26b6b63a4fc9b82cf084f9fedd
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-06-20 23:24:22 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,36 @@
 'use strict';
+
+class StringBuilder {
+  #value;
+  constructor(initialValue = '') {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+    return this;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+    return this;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+    return this;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
